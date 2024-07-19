72bab46f968d5bb4687a8e7d51155a22e7133377
Alexhr <alex.hernandez24kb@gmail.com>
2024-07-19 13:23:12 -0600

    PageView - App tutorial

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
+
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(
+    this.title, 
+    this.caption, 
+    this.imageUrl
+  );
+}
+
+final slides = <SlideInfo> [
+  SlideInfo('Busca la comida','Consequat minim esse enim enim ea laborum consectetur ea quis.' , 'assets/images/1.png'),
+  SlideInfo('Busca la comida','Esse aliquip pariatur pariatur esse sint velit tempor sint est sunt sit eiusmod.' , 'assets/images/2.png'),
+  SlideInfo('Busca la comida','Adipisicing consequat et ex labore sint dolor sit.' , 'assets/images/2.png'),
+];
+
 class AppTutorialScreen extends StatelessWidget {
 
   static const String name = 'tutorial_screen';
@@ -9,9 +29,34 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tutorial Screen'),
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map(
+          (slideData) => _Slide(
+          title: slideData.title, 
+          caption: slideData.caption, 
+          imageUrl: slideData.imageUrl
+       )
+        ).toList()
       ),
     );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title, 
+    required this.caption, 
+    required this.imageUrl
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
   }
 }

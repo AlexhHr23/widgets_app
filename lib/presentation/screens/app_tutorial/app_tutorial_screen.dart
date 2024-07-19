f7612b1bbf23162c12ec93ee33533e3f3d1c81d6
Alexhr <alex.hernandez24kb@gmail.com>
2024-07-19 14:16:08 -0600

    tutorial finalizado

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 
 
@@ -17,27 +19,87 @@ class SlideInfo {
 final slides = <SlideInfo> [
   SlideInfo('Busca la comida','Consequat minim esse enim enim ea laborum consectetur ea quis.' , 'assets/images/1.png'),
   SlideInfo('Busca la comida','Esse aliquip pariatur pariatur esse sint velit tempor sint est sunt sit eiusmod.' , 'assets/images/2.png'),
-  SlideInfo('Busca la comida','Adipisicing consequat et ex labore sint dolor sit.' , 'assets/images/2.png'),
+  SlideInfo('Busca la comida','Adipisicing consequat et ex labore sint dolor sit.' , 'assets/images/3.png'),
 ];
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
 
   static const String name = 'tutorial_screen';
+
+
   
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+
+  final PageController pageViewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageViewController.addListener(() { 
+
+      final page = pageViewController.page ?? 0;
+      if(!endReached && page >= (slides.length - 1.5)){
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: slides.map(
-          (slideData) => _Slide(
-          title: slideData.title, 
-          caption: slideData.caption, 
-          imageUrl: slideData.imageUrl
-       )
-        ).toList()
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageViewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides.map(
+              (slideData) => _Slide(
+              title: slideData.title, 
+              caption: slideData.caption, 
+              imageUrl: slideData.imageUrl
+           )).toList()
+          ),
+
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              child: const Text('Salir'),
+              onPressed: () => context.pop(),
+            ),
+          ),
+
+          endReached ? 
+          Positioned(
+            right: 30,
+            bottom: 50,
+            child: FadeInRight(
+              from: 15,
+              delay: const Duration(seconds: 1),
+              child: FilledButton(
+                onPressed: () => context.pop(),
+                child: const Text('Comenzar'),
+              ),
+            )
+          ): const SizedBox()
+        ],
       ),
     );
   }
@@ -57,6 +119,26 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+
+
+     final titleStyle  = Theme.of(context).textTheme.titleLarge;
+     final captionStyle  = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image( image: AssetImage(imageUrl)),
+            const SizedBox(height: 20),
+            Text(title, style: titleStyle),
+            const SizedBox(height: 10),
+            Text(caption, style: captionStyle)
+          ],
+        ),
+      ),
+    );
   }
 }
